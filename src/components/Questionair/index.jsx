1ade7e566b3b36f76b865162a53abc1c23c68881
qasimfida <qasimfida0054@gmail.com>
2022-01-11 21:52:28 +0500

    changes

--- src/components/Questionair/index.jsx
+++ src/components/Questionair/index.jsx
@@ -6,7 +6,7 @@ import Step2 from "./Step2";
 import Step3 from "./Step3";
 import Step4 from "./Step4";
 const Questionair = () => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(true)
   const [state, setstate] = useState({
     speaking: "",
     event: "",
